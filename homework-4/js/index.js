a8457c9fbcda5f59eaf8e490bb1eeebca52f6f1c
Chris Sauter <sauterdev@users.noreply.github.com>
2017-06-26 20:10:16 -0400

    JS homework final

--- homework-4/js/index.js
+++ homework-4/js/index.js
@@ -1,16 +1,18 @@
 console.log(5);
 //should change style to Nav links
-$("#navOption").click(styleLink);
+$("a").click(function(){
+  $("a").addClass("link");
+  console.log("testing");
+});
 
-function styleLink(){
-  $("#navOption").addClass("link");
-}
 //Pop up widow when you click the button
+/*
 $("#noInfo").click(popUpWindow);
 
 function popUpWindow(){
   alert("We arent ready for this info yet!");
 }
+*/
 
 //reveal mast
 
@@ -23,3 +25,51 @@ $("#reveal").click(function(){
   $(".mast").show(500);
   $("#reveal").removeClass("reveal");
 });
+
+//not ready for you info yet window
+
+$("#noInfo").click(function(){
+  $("#notReady").addClass("move-forward");
+});
+
+$("#removeWindow").click(function(){
+  $("#notReady").removeClass("move-forward");
+});
+/*
+$("#fade").mouseenter(function (){
+  $("#fade").hide(1000);
+  $("#fade").mouseleave(function (){
+    $("#fade").show(1000);
+  });
+});
+*/
+
+$("#readMore1").click(function(event){
+  event.preventDefault();
+  $("#moreBlog1").slideDown("slow");
+  $("#readMore1").hide();
+  $("#moreBlog1").show();
+})
+
+$("#readLess1").click(function(event){
+  event.preventDefault();
+  $('#moreBlog1').slideUp('slow');
+  $('#lessBlog1').hide();
+  $('#readMore1').show();
+})
+
+
+
+$("#readMore2").click(function(event){
+  event.preventDefault();
+  $("#moreBlog2").slideDown("slow");
+  $("#readMore2").hide();
+  $("#moreBlog2").show();
+})
+
+$("#readLess2").click(function(event){
+  event.preventDefault();
+  $('#moreBlog2').slideUp('slow');
+  $('#lessBlog2').hide();
+  $('#readMore2').show();
+})
